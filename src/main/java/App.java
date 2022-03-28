6b4a937f912d1876def731376fa6b50a06e0c603
Grigorii Lamantinov <grigoriiborodin@yandex.ru>
2022-03-28 17:21:07 +0300

    added dialog class, delete method, add method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,11 +1,42 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        TableMaker tableMaker = new TableMaker();
-        System.out.println("Hello");
+        TableCars tableMaker = new TableCars();
+        Dialog dialog = new Dialog();
         tableMaker.createTableCars();
+        Scanner sc = new Scanner(System.in);
+        boolean isExit = false;
 
-        while (!tableMaker.isClose()) {
-            tableMaker.dropTable();
+        System.out.println(dialog.actionMenu());
+        while (!isExit) {
+            System.out.println("\nТоварищ, что хотите сделать с данными?");
+            String action = sc.nextLine();
+            action = action.toUpperCase();
+
+            switch (action) {
+                case "ВЫВОД":
+                    System.out.println(tableMaker.outputData());
+                    break;
+                case "ДОБАВИТЬ":
+                    tableMaker.insertString(dialog.getData());
+                    break;
+                case "УДАЛИТЬ":
+                    tableMaker.deleteString(dialog.idForDelete());
+                    break;
+
+                case "МЕНЮ":
+                    System.out.println(dialog.actionMenu());
+                    break;
+                case "ВЫХОД":
+                    isExit = true;
+                    break;
+                default:
+                    System.out.println("Товарищ, такого мы сделать не можем");
+                    break;
+            }
         }
+
+        tableMaker.dropTable();
     }
 }
